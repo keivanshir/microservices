4e45f50c24d52e6484faa5d2033e0e3b900e2e4e
Seyyed Keivan Shirkoubian <keivan.shir.74@gmail.com>
2025-01-17 20:36:36 +0330

    -changed pom so only database accessed microservices use jpa -added gateway configurations

--- gateway/src/main/java/com/gateway/microservice/GatewayApp.java
+++ gateway/src/main/java/com/gateway/microservice/GatewayApp.java
@@ -2,8 +2,10 @@ package com.gateway.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
